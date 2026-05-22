07a52c742ddeb0a13d034fbb30342f4610a7c3ce
Dmitry Ananev <thesfinks.game@gmail.com>
2026-05-22 22:49:54 +0300

    set up database migrations

---
+++ migrations/settings.py
@@ -0,0 +1,42 @@
+import dotenv
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE = dotenv.find_dotenv()
+
+
+class PostgresSettings(BaseModel):
+    protocol: str = "postgresql+asyncpg"
+    host: str = "localhost"
+    port: int = 5432
+    user: str = "123"
+    password: str = "postgres"
+    db: str = "main"
+
+    @property
+    def dsn(self) -> str:
+        return f"{self.protocol}://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
+    @property
+    def dsn_safe(self) -> str:
+        return f"{self.protocol}://{self.user}:****@{self.host}:{self.port}/{self.db}"
+
+
+class Settings(BaseSettings):
+    postgres: PostgresSettings = PostgresSettings()
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        extra="ignore",
+        env_file_encoding="utf-8",
+    )
+
+
+def _get_setting() -> Settings:
+    return Settings()
+
+
+settings = _get_setting()
---
+++ services/infrastructure/database/base_model.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    repr_cols_num = 3
+    repr_cols = ()
+
+    def __repr__(self):
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
